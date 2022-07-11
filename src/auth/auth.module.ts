4094b99c61c1c955c19564f176a02890f97cc401
Joao Henrique <henry29amaral@gmail.com>
2022-07-11 17:44:09 -0300

    Implemented login, guard and localStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { UsersService } from 'src/users/users.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UsersService, PrismaService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly UsersService: UsersService) {}
+
+  async validateUser(email: string, password: string) {
+    return null;
+  }
+}
